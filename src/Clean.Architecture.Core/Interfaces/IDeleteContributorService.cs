7ee20de4428d60e6d9b8df70d010413a3faa8ec8
HaoChen <lihaochen987@gmail.com>
2023-02-03 18:44:37 +1300

    Added documentation to IDeleteContributorService.cs

--- src/Clean.Architecture.Core/Interfaces/IDeleteContributorService.cs
+++ src/Clean.Architecture.Core/Interfaces/IDeleteContributorService.cs
@@ -1,8 +1,16 @@
-using Ardalis.Result;
-
 namespace Clean.Architecture.Core.Interfaces;
 
+using Ardalis.Result;
+
+/// <summary>
+/// The Interface of the DeleteContributorService.
+/// </summary>
 public interface IDeleteContributorService
 {
-    public Task<Result> DeleteContributor(int contributorId);
+  /// <summary>
+  /// The method responsible for deleting the Contributor.
+  /// </summary>
+  /// <param name="contributorId">The Id of the Contributor being deleted.</param>
+  /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+  public Task<Result> DeleteContributor(int contributorId);
 }
