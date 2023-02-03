46bed5ced71c622e489f885383f6be35adfa275f
HaoChen <lihaochen987@gmail.com>
2023-02-03 18:49:07 +1300

    Add documentation to SmtpEmailSender.cs

--- src/Clean.Architecture.Infrastructure/SmtpEmailSender.cs
+++ src/Clean.Architecture.Infrastructure/SmtpEmailSender.cs
@@ -1,29 +1,35 @@
-﻿using System.Net.Mail;
-using Clean.Architecture.Core.Interfaces;
+﻿namespace Clean.Architecture.Infrastructure;
+
+using System.Net.Mail;
+using Core.Interfaces;
 using Microsoft.Extensions.Logging;
 
-namespace Clean.Architecture.Infrastructure;
-
+/// <summary>
+/// The actual implementation of the EmailSender service.
+/// </summary>
 public class SmtpEmailSender : IEmailSender
 {
+  /// <summary>
+  /// The logger.
+  /// </summary>
   private readonly ILogger<SmtpEmailSender> _logger;
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="SmtpEmailSender"/> class.
+  /// </summary>
+  /// <param name="logger">The logger object.</param>
   public SmtpEmailSender(ILogger<SmtpEmailSender> logger)
   {
-    _logger = logger;
+    this._logger = logger;
   }
 
+  /// <inheritdoc/>
   public async Task SendEmailAsync(string to, string from, string subject, string body)
   {
     var emailClient = new SmtpClient("localhost");
-    var message = new MailMessage
-    {
-      From = new MailAddress(from),
-      Subject = subject,
-      Body = body
-    };
+    var message = new MailMessage { From = new MailAddress(from), Subject = subject, Body = body };
     message.To.Add(new MailAddress(to));
     await emailClient.SendMailAsync(message);
-    _logger.LogWarning("Sending email to {to} from {from} with subject {subject}.", to, from, subject);
+    _logger.LogWarning("Sending email to {To} from {From} with subject {Subject}", to, from, subject);
   }
 }
