6ae0427b5178c10beddf0ee7cea216a3f6c0b12d
HaoChen <lihaochen987@gmail.com>
2023-02-03 17:51:35 +1300

    Fix Documentation for IAggregateRoot.cs

--- src/Clean.Architecture.SharedKernel/Interfaces/IAggregateRoot.cs
+++ src/Clean.Architecture.SharedKernel/Interfaces/IAggregateRoot.cs
@@ -2,8 +2,8 @@
 
 /// <summary>
 /// The cluster of domain objects to be treated as a single unit.
-/// Apply this marker interface only to aggregate root entities
-/// Repositories will only work with aggregate roots, not their children
+/// Apply this marker interface only to aggregate root entities.
+/// Repositories will only work with aggregate roots, not their children.
 /// </summary>
 public interface IAggregateRoot
 {
