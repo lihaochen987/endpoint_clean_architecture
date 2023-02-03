97deaddcf79ab3eca97c29d24870923f5da5c72f
HaoChen <lihaochen987@gmail.com>
2023-02-03 18:43:42 +1300

    Added documentation to ContributorDeletedEvent.cs

--- src/Clean.Architecture.Core/ContributorAggregate/Events/ContributorDeletedEvent.cs
+++ src/Clean.Architecture.Core/ContributorAggregate/Events/ContributorDeletedEvent.cs
@@ -1,13 +1,23 @@
-﻿using Clean.Architecture.SharedKernel;
+﻿namespace Clean.Architecture.Core.ContributorAggregate.Events;
 
-namespace Clean.Architecture.Core.ContributorAggregate.Events;
+using SharedKernel;
 
+/// <summary>
+/// An event where a Contributor is deleted.
+/// </summary>
 public class ContributorDeletedEvent : DomainEventBase
 {
-  public int ContributorId { get; set; }
-
+  /// <summary>
+  /// Initializes a new instance of the <see cref="ContributorDeletedEvent"/> class.
+  /// </summary>
+  /// <param name="contributorId">The Id of the Contributor.</param>
   public ContributorDeletedEvent(int contributorId)
   {
     ContributorId = contributorId;
   }
+
+  /// <summary>
+  /// Gets or sets the Id of the Contributor that was deleted.
+  /// </summary>
+  public int ContributorId { get; set; }
 }
