4605d8369ad4ee44fef3f947be2192693e3a01f7
HaoChen <lihaochen987@gmail.com>
2023-02-03 17:58:52 +1300

    Add documentation to Contributor.cs

--- src/Clean.Architecture.Core/ContributorAggregate/Contributor.cs
+++ src/Clean.Architecture.Core/ContributorAggregate/Contributor.cs
@@ -1,18 +1,32 @@
-﻿using Ardalis.GuardClauses;
-using Clean.Architecture.SharedKernel;
+﻿namespace Clean.Architecture.Core.ContributorAggregate;
+
+using Ardalis.GuardClauses;
+using SharedKernel;
 using Clean.Architecture.SharedKernel.Interfaces;
 
-namespace Clean.Architecture.Core.ContributorAggregate;
-
+/// <summary>
+/// The Contributor Domain Model Object.
+/// </summary>
 public class Contributor : EntityBase, IAggregateRoot
 {
-  public string Name { get; private set; }
-
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Contributor"/> class.
+  /// </summary>
+  /// <param name="name">The Name of the contributor.</param>
   public Contributor(string name)
   {
     Name = Guard.Against.NullOrEmpty(name, nameof(name));
   }
 
+  /// <summary>
+  /// Gets the Name of the Contributor.
+  /// </summary>
+  public string Name { get; private set; }
+
+  /// <summary>
+  /// Updates the Name of the contributor.
+  /// </summary>
+  /// <param name="newName">The new Name of the contributor.</param>
   public void UpdateName(string newName)
   {
     Name = Guard.Against.NullOrEmpty(newName, nameof(newName));
