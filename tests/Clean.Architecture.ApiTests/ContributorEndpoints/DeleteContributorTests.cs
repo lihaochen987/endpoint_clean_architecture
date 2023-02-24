7ddb1d03a6fff6f579ff5574eb240fbed6b42062
HaoChen <lihaochen987@gmail.com>
2023-02-25 07:07:47 +1300

    Initialised Update and Delete Contributor API tests

---
+++ tests/Clean.Architecture.ApiTests/ContributorEndpoints/DeleteContributorTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Clean.Architecture.ApiTests.ContributorEndpoints;
+
+using Xunit;
+using Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+/// <summary>
+/// Tests relating to the DeleteContributor endpoint.
+/// </summary>
+[Collection("Sequential")]
+public class DeleteContributorTests : IClassFixture<CustomWebApplicationFactory<WebMarker>>
+{
+  private readonly HttpClient _client;
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="DeleteContributorTests"/> class.
+  /// </summary>
+  /// <param name="factory">The client used for testing purposes.</param>
+  public DeleteContributorTests(WebApplicationFactory<WebMarker> factory)
+  {
+    _client = factory.CreateClient();
+  }
+}
---
+++ tests/Clean.Architecture.ApiTests/ContributorEndpoints/UpdateContributorTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Clean.Architecture.ApiTests.ContributorEndpoints;
+
+using Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+/// <summary>
+/// Tests relating to the UpdateContributor endpoint.
+/// </summary>
+[Collection("Sequential")]
+public class UpdateContributorTests : IClassFixture<CustomWebApplicationFactory<WebMarker>>
+{
+  private readonly HttpClient _client;
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="UpdateContributorTests"/> class.
+  /// </summary>
+  /// <param name="factory">The client used for testing purposes.</param>
+  public UpdateContributorTests(WebApplicationFactory<WebMarker> factory)
+  {
+    _client = factory.CreateClient();
+  }
+}
