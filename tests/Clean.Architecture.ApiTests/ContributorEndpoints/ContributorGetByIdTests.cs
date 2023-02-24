0af8ebed9ecbfafcb91be63f3727995a6875e7cc
HaoChen <lihaochen987@gmail.com>
2023-02-24 19:57:30 +1300

    Polished up ContributorGetByIdTests.cs

--- tests/Clean.Architecture.ApiTests/ContributorEndpoints/ContributorGetByIdTests.cs
+++ tests/Clean.Architecture.ApiTests/ContributorEndpoints/ContributorGetByIdTests.cs
@@ -5,9 +5,10 @@ using Infrastructure.Data;
 using Web;
 using Clean.Architecture.Web.ContributorEndpoints;
 using Xunit;
+using Shouldly;
 
 /// <summary>
-/// TODO.
+/// Tests relating to the GetContributorById endpoint.
 /// </summary>
 [Collection("Sequential")]
 public class ContributorGetByIdTests : IClassFixture<CustomWebApplicationFactory<WebMarker>>
@@ -17,33 +18,45 @@ public class ContributorGetByIdTests : IClassFixture<CustomWebApplicationFactory
   /// <summary>
   /// Initializes a new instance of the <see cref="ContributorGetByIdTests"/> class.
   /// </summary>
-  /// <param name="factory">TODO.</param>
+  /// <param name="factory">The client used for testing purposes.</param>
   public ContributorGetByIdTests(CustomWebApplicationFactory<WebMarker> factory)
   {
     _client = factory.CreateClient();
   }
 
   /// <summary>
-  /// TODO.
+  /// Ensures that the previously seeded Contributor is successfully retrieved.
   /// </summary>
   /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
   [Fact]
   public async Task ReturnsSeedContributorGivenId1()
   {
-    var result = await _client.GetAndDeserializeAsync<ContributorRecord>(GetContributorByIdRequest.BuildRoute(1));
+    // Arrange
+    const int seededId = 1;
 
-    Assert.Equal(1, result.id);
-    Assert.Equal(AppDbContextSeed.Contributor1.Name, result.name);
+    // Act
+    var result =
+      await _client.GetAndDeserializeAsync<ContributorRecord>(GetContributorByIdRequest.BuildRoute(seededId));
+
+    // Assert
+    result.id.ShouldBe(1);
+    result.name.ShouldBe(AppDbContextSeed.Contributor1.Name);
   }
 
   /// <summary>
-  /// TODO.
+  /// Ensures an invalid Contributor is not found.
   /// </summary>
   /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
   [Fact]
   public async Task ReturnsNotFoundGivenId0()
   {
-    string route = GetContributorByIdRequest.BuildRoute(0);
+    // Arrange
+    const int invalidSeedId = 0;
+
+    // Act
+    string route = GetContributorByIdRequest.BuildRoute(invalidSeedId);
+
+    // Assert
     _ = await _client.GetAndEnsureNotFoundAsync(route);
   }
 }
