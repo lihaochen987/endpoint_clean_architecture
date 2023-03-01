9fdb6de55ca2245b46cc46972c34686954a59e9f
HaoChen <lihaochen987@gmail.com>
2023-03-01 13:20:07 +1300

    Rename id and name to contributorId and contributorName

--- src/Clean.Architecture.Api/ContributorEndpoints/ContributorRecord.cs
+++ src/Clean.Architecture.Api/ContributorEndpoints/ContributorRecord.cs
@@ -3,6 +3,6 @@
 /// <summary>
 /// The shared response class of ContributorEndpoints.
 /// </summary>
-/// <param name="id">The Id of the contributor.</param>
-/// <param name="name">The name of the contributor.</param>
-public record ContributorRecord(int id, string name);
+/// <param name="contributorId">The Id of the contributor.</param>
+/// <param name="contributorName">The name of the contributor.</param>
+public record ContributorRecord(int contributorId, string contributorName);
