8cfdff2bb072b1b49f2f57e322c84a1b3602b32d
HaoChen <lihaochen987@gmail.com>
2023-03-01 13:31:29 +1300

    Add UpdateContributorTests

--- tests/Clean.Architecture.ApiTests/ContributorEndpoints/UpdateContributorTests.cs
+++ tests/Clean.Architecture.ApiTests/ContributorEndpoints/UpdateContributorTests.cs
@@ -1,8 +1,11 @@
 ﻿namespace Clean.Architecture.ApiTests.ContributorEndpoints;
 
 using Web;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using System.Net;
+using Clean.Architecture.Web.ContributorEndpoints;
+using FastEndpoints;
+using Shouldly;
 
 /// <summary>
 /// Tests relating to the UpdateContributor endpoint.
@@ -16,8 +19,67 @@ public class UpdateContributorTests : IClassFixture<CustomWebApplicationFactory<
   /// Initializes a new instance of the <see cref="UpdateContributorTests"/> class.
   /// </summary>
   /// <param name="factory">The client used for testing purposes.</param>
-  public UpdateContributorTests(WebApplicationFactory<WebMarker> factory)
+  public UpdateContributorTests(CustomWebApplicationFactory<WebMarker> factory)
   {
     _client = factory.CreateClient();
   }
+
+  /// <summary>
+  /// Ensures that a Contributor in the database is successfully updated.
+  /// </summary>
+  /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+  [Fact]
+  public async Task ValidContributorIsUpdated()
+  {
+    // Arrange
+    var request = new UpdateContributorRequest { Id = 1, Name = "New Ardalis" };
+
+    // Act
+    var (response, result) =
+      await _client.PUTAsync<UpdateContributor, UpdateContributorRequest, UpdateContributorResponse>(request);
+
+    // Assert
+    response.ShouldNotBeNull();
+    response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    result?.Contributor?.contributorId.ShouldBe(1);
+    result?.Contributor?.contributorName.ShouldBe("New Ardalis");
+  }
+
+  /// <summary>
+  /// Ensures that a request with no name returns a Bad Request response.
+  /// </summary>
+  /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+  [Fact]
+  public async Task NoNameReturnsBadRequest()
+  {
+    // Arrange
+    var request = new UpdateContributorRequest { Id = 1 };
+
+    // Act
+    var (response, _) =
+      await _client.PUTAsync<UpdateContributor, UpdateContributorRequest, UpdateContributorResponse>(request);
+
+    // Assert
+    response.ShouldNotBeNull();
+    response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+  }
+
+  // /// <summary>
+  // /// Uncomment and fix.
+  // /// </summary>
+  // /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+  // [Fact]
+  // public async Task NonExistingContributorReturnsNotFound()
+  // {
+  //   // Arrange
+  //   var request = new UpdateContributorRequest { Id = 0, Name = "Doesnt Exist" };
+  //
+  //   // Act
+  //   var (response, _) =
+  //     await _client.PUTAsync<UpdateContributor, UpdateContributorRequest, UpdateContributorResponse>(request);
+  //
+  //   // Assert
+  //   response.ShouldNotBeNull();
+  //   response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+  // }
 }
